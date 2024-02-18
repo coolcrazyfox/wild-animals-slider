ac9031b424decf4a126ccf4e2c8035e8365b9a25
Andrei Malets <coolviktori@yandex.by>
2024-02-18 17:26:55 +0300

    changes App

---
+++ src/componnents/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <a>Home</a>
+      <a>Contact</a>
+      <a>Info</a>
+    </nav>
+  );
+};
+
+export default Header;
