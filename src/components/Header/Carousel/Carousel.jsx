839689a4db440401703a6277e3574ca1b279e258
Andrei Malets <coolviktori@yandex.by>
2024-03-01 20:59:47 +0300

    changes Img

--- src/components/Header/Carousel/Carousel.jsx
+++ src/components/Header/Carousel/Carousel.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-import img1 from "../../../accent/img1.jpg";
-import img2 from "../../../accent/img2.jpg";
-import img3 from "../../../accent/img3.jpg";
-import img4 from "../../../accent/img4.jpg";
-import img5 from "../../../accent/img5.jpg";
-import img6 from "../../../accent/img6.jpg";
+import img1 from "../../../accent/img9.jpg";
+import img2 from "../../../accent/img10.jpg";
+import img3 from "../../../accent/img11.jpg";
+import img4 from "../../../accent/img12.jpg";
+import img5 from "../../../accent/img13.jpg";
+import img6 from "../../../accent/img14.jpg";
+import img7 from "../../../accent/img15.jpg";
+import img8 from "../../../accent/img16.jpg";
+import img9 from "../../../accent/img17.jpg";
+import img10 from "../../../accent/img18.jpg";
+import img11 from "../../../accent/img19.jpg";
+import img12 from "../../../accent/img20.jpg";
+import img13 from "../../../accent/img21.jpg";
+import img14 from "../../../accent/img22.jpg";
 import AnimalItem from "./AnimalItem";
 import style from "../../../styles/Carousel.module.css";
 import Thumbnail from "../../Thumbnail/Thumbnail";
 
 const Carousel = () => {
+  // const initState = [
+  //   { id: 1, name: "elk", images: img1 },
+  //   { id: 2, name: "elephant", images: img2 },
+  //   { id: 3, name: "leopard", images: img3 },
+  //   { id: 4, name: "wolf", images: img4 },
+  //   { id: 5, name: "deer", images: img5 },
+  //   { id: 6, name: "lion", images: img6 },
+  // ];
   const initState = [
     { id: 1, name: "elk", images: img1 },
     { id: 2, name: "elephant", images: img2 },
@@ -18,7 +34,6 @@ const Carousel = () => {
     { id: 5, name: "deer", images: img5 },
     { id: 6, name: "lion", images: img6 },
   ];
-
   return (
     <div className={style.carousel}>
       {initState.map((a, i) => {
