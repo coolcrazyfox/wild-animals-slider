5cd870e7d8eb73dfe6b5825ffa8986179e2ca460
Andrei Malets <coolviktori@yandex.by>
2024-03-03 17:07:01 +0300

    changes CarsItem

--- src/components/Thumbnail/Thumbnail.jsx
+++ src/components/Thumbnail/Thumbnail.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import style from "../../styles/Thumbnail.module.css";
 
 const Thumbnail = ({ card }) => {
+  const [imgIndex, setImgIndex] = React.useState(0);
+
   return (
     <>
-      <div className={style.item}>
+      <div className={style.item} onClick={() => setImgIndex(imgIndex + 1)}>
         <img className={style.img} src={card.images} />
         <div className={style.content}>
           <div className={style.title}>
             {" "}
             {card.name}
-            {card.id}
+            {card.id}-{imgIndex}
           </div>
           <div className={style.des}> Description {card.id}</div>
         </div>
