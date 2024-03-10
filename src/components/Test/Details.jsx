cd5a858127375976629b5ce0f2511b848c307d84
Andrei Malets <coolviktori@yandex.by>
2024-03-10 18:08:47 +0300

    create Details

---
+++ src/components/Test/Details.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Details = ({ title }) => {
+  return (
+    <div>
+      <h1>Hi , guys!</h1>
+      <p>{title}</p>
+      <button>Click</button>
+    </div>
+  );
+};
+
+export default Details;
