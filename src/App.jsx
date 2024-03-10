1029920987b9e55c75789dcc0378f5383bd05755
Andrei Malets <coolviktori@yandex.by>
2024-03-10 18:29:26 +0300

    CHANGES dETAILS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import React, { useState } from "react";
 import Carousel from "./components/Header/Carousel/Carousel";
 import Header from "./components/Header/Header";
+import Details from "./components/Test/Details";
 import s from "./styles/App.module.css";
 
 function App() {
+  const [details, setDetails] = useState({
+    title: "text",
+    description: "History, guys",
+    btnText: "Click",
+  });
   return (
     <div className={s.App}>
-      <Details />
+      <Details details={details} setDetails={setDetails} />
     </div>
     //  <div className={s.App}>
     // <Header />
--- src/components/Test/Details.jsx
+++ src/components/Test/Details.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-const Details = ({ title }) => {
+const Details = ({ details, setDetails }) => {
   return (
     <div>
-      <h1>Hi , guys!</h1>
-      <p>{title}</p>
-      <button>Click</button>
+      <h1>{details.title}</h1>
+      <p>{details.description}</p>
+      <button
+        onClick={() =>
+          setDetails((prev) => {
+            return { ...prev, title: "New title" };
+          })
+        }
+      >
+        {details.btnText}
+      </button>
     </div>
   );
 };
